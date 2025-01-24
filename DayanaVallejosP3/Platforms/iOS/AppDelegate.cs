e3661b5e67b5b5c3f0883ae8dbd322a1be201615
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2025-01-24 07:14:45 -0500

    Add project files.

---
+++ DayanaVallejosP3/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DayanaVallejosP3
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
