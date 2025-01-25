3a9e0f816d4ee7fc912f62f6455eadf7d865e2c8
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2025-01-24 23:29:32 -0500

    Interface Commit

---
+++ DayanaVallejosP3/Servicios/IDatabaseService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DayanaVallejosP3.Models;
+
+namespace DayanaVallejosP3.Servicios
+{
+    public interface IDatabaseService
+    {
+        Task SaveAeropuertoAsync(Aeropuerto aeropuerto);
+        Task<List<Aeropuerto>> GetAeropuertosAsync();
+    }
+}
